c76f435535c9b57c1440e2b7d553eb5754fd04d5
Fabio <fabio.scognamiglio@studio.unibo.it>
2019-05-03 17:34:05 +0200

    change PlayerImpl.java

--- src/player/PlayerImpl.java
+++ src/player/PlayerImpl.java
@@ -1,7 +1,50 @@
-package Player;
+package player;
 
-public class PlayerImpl {
+import utility.Colors;
+import utility.Pawns;
 
+public class PlayerImpl implements Player {
+
+    private String name;
+    private Colors color;
+    private Pawns pawns;
+    
+    public PlayerImpl(String name, Colors color, Pawns pawns) {
+        
+        this.name = name;
+        this.color = color;
+        this.pawns = pawns;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Colors getColors() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+    
+    public Pawns getPawn() {
+        return pawns;
+    }
+    
+    public void setPawns(Pawns pawns) {
+        this.pawns = pawns;
+    }
+
+  
+
+
+    
+    
     
     
     
