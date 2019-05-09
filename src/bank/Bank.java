f861701d9463347756984b6ef0b801c080363896
Andrea Tassani <andrea.tassani@studio.unibo.it>
2019-05-09 15:53:54 +0200

    Bank interface and classes created

---
+++ src/bank/Bank.java
@@ -0,0 +1,15 @@
+package bank;
+
+import properties.PropertiesCardsImpl;
+/**
+ * 
+ * interface of the bank.
+ *
+ */
+public interface Bank {
+    /**
+     * @param c
+     * method to add card to the bank at start of the game.
+     */
+    void addCard(PropertiesCardsImpl c);
+    }
---
+++ src/bank/BankImpl.java
@@ -0,0 +1,19 @@
+package bank;
+
+import java.util.ArrayList;
+import properties.PropertiesCardsImpl;
+/**
+ * {@inheritDoc}.
+ */
+public class BankImpl implements Bank {
+    
+    private ArrayList<PropertiesCardsImpl> allProperties = new ArrayList<PropertiesCardsImpl>();
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public void addCard(final PropertiesCardsImpl c) {
+       allProperties.add(c);
+    }
+
+}
