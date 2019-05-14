34de26b32819ba4c8ad4f1196eec9e1074b1d8fc
ENRICO <enrico.roncuzzi@studio.unibo.it>
2019-05-14 15:13:22 +0200

    change history class

--- src/history/HistoryGUI.java
+++ src/history/HistoryGUI.java
@@ -13,20 +13,54 @@ public class HistoryGUI extends JFrame{
     public HistoryGUI() {
         final JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setSize(250, 250);
        
         JPanel panel = new JPanel(new BorderLayout());
+        //pannello principale
         
-        final JPanel sopra = new JPanel(new FlowLayout());
-       final JTextField campo = new JTextField("History");
-       sopra.add(campo, FlowLayout.LEFT);
-        panel.add(sopra,BorderLayout.NORTH);
+        JPanel pannello = new JPanel();
+        pannello.setLayout(new BoxLayout(pannello, BoxLayout.PAGE_AXIS));
         
         
-        final JTextArea textArea = new JTextArea (); // Area di testo
-         textArea . setLineWrap ( true );
-        final JScrollPane scroll = new JScrollPane ( textArea ); // Pannello con barra
+        
+        final JTextField textField1 = new JTextField ("A chicco");
+        textField1.setBounds();
+        final JTextField textField2 = new JTextField ("bleah");
+        final JTextField textField3 = new JTextField ("A fef");
+        final JTextField textField4 = new JTextField ("A chiccfegro");
+        final JTextField textField5 = new JTextField ("A chiccfvdscvegro");
+        final JTextField textField6 = new JTextField ("bleah");
+        final JTextField textField7 = new JTextField ("A fef");
+        final JTextField textField8 = new JTextField ("A chiccfegro");
+        final JTextField textField9 = new JTextField ("A chiccfvdscvegro");
+        
+        
+       
+         pannello.add(textField1);
+         pannello.add(textField2);
+         pannello.add(textField3);
+         pannello.add(textField4);
+         pannello.add(textField5);
+         pannello.add(textField6);
+         pannello.add(textField7);
+         pannello.add(textField8);
+         pannello.add(textField9);
+         
+        
+        final JScrollPane scroll = new JScrollPane (pannello); 
         scroll . setVerticalScrollBarPolicy ( ScrollPaneConstants . VERTICAL_SCROLLBAR_ALWAYS );
+        
+      
+       
+       //setto la barra con scroll
+        
+//        final JPanel sopra = new JPanel(new FlowLayout());
+//       final JTextField campo = new JTextField("History");
+//       sopra.add(campo, FlowLayout.LEFT);
+//        panel.add(sopra,BorderLayout.NORTH);
+        
+        
+        
         
         panel.add(scroll,BorderLayout.CENTER);
         
