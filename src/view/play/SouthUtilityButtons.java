850d22244bd29bff8ad02fa369c4a4a7abb30cf3
Michele Torroni <michele.torroni@studio.unibo.it>
2019-08-06 02:31:22 +0200

    added rollDice button and actionListener

--- src/view/play/SouthUtilityButtons.java
+++ src/view/play/SouthUtilityButtons.java
@@ -3,29 +3,50 @@ package view.play;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Random;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 public class SouthUtilityButtons extends JPanel {
+    
 	
 	public SouthUtilityButtons() {
 		this.setLayout(new GridLayout());
 		
+		JButton rollDice = new JButton("ROLL DICE");
 		JButton sell = new JButton("SELL");
 		JButton buy = new JButton("BUY");
 		JButton menù = new JButton("MENU'");
 		JButton build = new JButton("BUILD");
+		JButton nextPlayer = new JButton("NEXT PLAYER");
 		
+		this.add(rollDice);
 		this.add(sell);
 		this.add(buy);
 		this.add(build);
 		this.add(menù);
-
+		this.add(nextPlayer);
+		
+		rollDice.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        Random r = new Random();
+                        rollDice.setEnabled(false);
+                        int risultato = r.nextInt(5)+1;
+                        JOptionPane.showMessageDialog(null,"il risultato è stato:"+risultato, "Tiro del dado", 
+                                                      JOptionPane.INFORMATION_MESSAGE, new ImageIcon("res/dado"+risultato+".png "));
+                        
+                    }
+                });
 		this.setBorder(new LineBorder(Color.BLACK));
 	}
 
