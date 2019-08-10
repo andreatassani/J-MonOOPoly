80fb55be38d40ae6aa099a1d2bb41fa9e6f19eb8
ENRICO RONCUZZI <enrico.roncuzzi@studio.unibo.it>
2019-08-10 16:34:20 +0200

    Working on GUI

--- src/view/play/HistoryGUI.java
+++ src/view/play/HistoryGUI.java
@@ -4,28 +4,39 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.Toolkit;
+import java.util.ArrayList;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
+
+import controller.menu.ControllerMainMenu;
 
 public class HistoryGUI extends JPanel {
 	private static final int width = Toolkit.getDefaultToolkit().getScreenSize().width;
 	private static final int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+	
 	private static Dimension leftSideDimension = new Dimension ((int) (width / 1.45),(height /2));
 	private static Dimension rightSideDimension = new Dimension((int) (width - leftSideDimension.getSize().width),(height/2));
-	private static Dimension dim = new Dimension((int) (rightSideDimension.getSize().width), (rightSideDimension.getSize().height/7));
+	private static Dimension dim = new Dimension((int) (rightSideDimension.getSize().width), (rightSideDimension.getSize().height/12));
+	private static final Font f = new Font("Aldhabi", Font.LAYOUT_LEFT_TO_RIGHT,rightSideDimension.getSize().height/12);
+	private setComponent action;
 	
-	private final Box box;
+	private final ArrayList<JButton> fields = new ArrayList<JButton>();
+	private final JPanel box;
 	private final JScrollPane scrollPanel;
 	private final JPanel gridPanel;
 	private final JPanel upPanelLeft;
@@ -36,20 +47,17 @@ public class HistoryGUI extends JPanel {
 			
 	public HistoryGUI() {
 		
+		this.action = new setComponentImpl();
 		this.setLayout(new BorderLayout());
 		this.setSize(rightSideDimension);
-	//	this.setMaximumSize(rightSideDimension);
-	//	this.setMinimumSize(rightSideDimension);
 		this.setPreferredSize(rightSideDimension);
+		this.box= new JPanel(new GridLayout(0,1));
 		
-		this.box= new Box(BoxLayout.Y_AXIS);
-		this.scrollPanel = new JScrollPane (this.box);
-		scrollPanel.setVerticalScrollBarPolicy ( ScrollPaneConstants . VERTICAL_SCROLLBAR_ALWAYS );
 		
-		System.out.println("S" + leftSideDimension.toString());
-		System.out.println("D" +rightSideDimension.toString());
-		System.out.println("TOT " + Toolkit.getDefaultToolkit().getScreenSize().toString());
-		System.out.println("dim " + dim.toString());
+//		System.out.println("screen " + Toolkit.getDefaultToolkit().getScreenSize());
+//		System.out.println("dim. " + dim);
+//		System.out.println("left. " + leftSideDimension);
+		
 
 		
 		
@@ -58,26 +66,40 @@ public class HistoryGUI extends JPanel {
 	    this.upPanelLeft = new JPanel(new FlowLayout(FlowLayout.LEFT));
 	    this.upPanelRight = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 	    
+	    
 	    this.history = new JTextField("History");
 	    this.time = new JTextField("History");
 	    
-		
+		int i =0;
+		  for(i=0; i<3 ;i++) {
+		  final JButton but1 = new JButton ("E' iniziato il gioco!");
+		  but1.setSize(dim);
+		  but1.setPreferredSize(dim);
+		  but1.setMaximumSize(dim);
+		  but1.setMinimumSize(dim);
+	      but1.setHorizontalAlignment(SwingConstants.LEFT);
+	      but1.setFont(f);
+	      this.box.add(but1);
+		  }
 		  
-		  final JTextField textField1 = new JTextField ("A chicco");
-	      final JTextField textField2 = new JTextField ("bleah");
-	      final JTextField textField3 = new JTextField ("A fef");
-	      final JTextField textField4 = new JTextField ("A chiccfegro");
-	      
-	      
-	      this.box.add(textField4);
+		
+
+		 
+	   
 
 		
 	        upPanelLeft.add(history);
 	        upPanelRight.add(time);
 	        gridPanel.add(upPanelLeft);
 	        gridPanel.add(upPanelRight);
+	       
 	        
-	  this.add(scrollPanel,BorderLayout.CENTER);
+	        
+	        this.scrollPanel = new JScrollPane (this.box);
+			this.scrollPanel.setVerticalScrollBarPolicy ( ScrollPaneConstants . VERTICAL_SCROLLBAR_ALWAYS );	
+			
+			
+		this.add(scrollPanel,BorderLayout.CENTER);
 	    this.add(gridPanel,BorderLayout.NORTH);
    
 		this.setBorder(new LineBorder(Color.BLACK));
