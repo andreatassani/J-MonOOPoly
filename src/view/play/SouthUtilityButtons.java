df77947c9b4279f7fb65d86a1fd5eb103fed1d69
Michele Torroni <michele.torroni@studio.unibo.it>
2019-08-14 15:24:55 +0200

    finished pawns movement

--- src/view/play/SouthUtilityButtons.java
+++ src/view/play/SouthUtilityButtons.java
@@ -101,34 +101,66 @@ public class SouthUtilityButtons extends JPanel {
                                 System.err.println(er.getMessage());
                             }
                         }
-                        ((Cel)grid.getEastBox().getComponent(pos)).getPositionPawns().resetPawnOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1);
-                        ((Cel)grid.getEastBox().getComponent(risultato+pos)).getPositionPawns().setImageOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1, listPl.getCurrentPlayer().getPawn());
                         
-                        listPl.getCurrentPlayer().setPosition(pos+risultato);
+                        if(pos>=0 && pos <=10) {
+                        ((Cel)grid.getNorthBox().getComponent(pos)).getPositionPawns().resetPawnOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1);
+                        } else if (pos>=11 && pos<=19){
+                        ((Cel)grid.getEastBox().getComponent(pos-11)).getPositionPawns().resetPawnOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1);
+                        } else if(pos>=20 && pos<=30) {
+                        ((Cel)grid.getSouthBox().getComponent(30-pos)).getPositionPawns().resetPawnOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1);    
+                        } else if(pos>=31 && pos<=39) {
+                        ((Cel)grid.getWestBox().getComponent(39-pos)).getPositionPawns().resetPawnOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1);       
+                        }
+                       
+                        if(pos+risultato >=40) {
+                            listPl.getCurrentPlayer().setPosition(pos+risultato-40);
+                            listPl.getCurrentPlayer().setMoney(200);
+                          //Da togliere
+                            JOptionPane.showMessageDialog(null,"il giocatore "+listPl.getCurrentPlayer().getName()+" è passato dal via e guadagna 200 $, ora possiede "+listPl.getCurrentPlayer().getMoney()+"$",
+                                    "messaggio", 0);
+                        } else {
+                            listPl.getCurrentPlayer().setPosition(pos+risultato);
+                        }
+                        
+                        if(listPl.getCurrentPlayer().getPosition()>=0 && listPl.getCurrentPlayer().getPosition()<=10) {
+                        ((Cel)grid.getNorthBox().getComponent(listPl.getCurrentPlayer().getPosition())).getPositionPawns().setImageOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1, listPl.getCurrentPlayer().getPawn());
+                        } else if (listPl.getCurrentPlayer().getPosition()>=11 && listPl.getCurrentPlayer().getPosition()<=19){
+                        ((Cel)grid.getEastBox().getComponent(listPl.getCurrentPlayer().getPosition()-11)).getPositionPawns().setImageOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1, listPl.getCurrentPlayer().getPawn());
+                        } else if(listPl.getCurrentPlayer().getPosition()>=20 && listPl.getCurrentPlayer().getPosition()<=30) {
+                        ((Cel)grid.getSouthBox().getComponent(30-listPl.getCurrentPlayer().getPosition())).getPositionPawns().setImageOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1, listPl.getCurrentPlayer().getPawn());
+                        } else if(listPl.getCurrentPlayer().getPosition()>=31 && listPl.getCurrentPlayer().getPosition()<=39) {
+                        ((Cel)grid.getWestBox().getComponent(39-listPl.getCurrentPlayer().getPosition())).getPositionPawns().setImageOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1, listPl.getCurrentPlayer().getPawn());
+                        }
+                        
                         
                         //Da togliere
-                        JOptionPane.showMessageDialog(null,"il giocatore "+listPl.getCurrentPlayer().getName()+" è finito sulla casella "+deck.get(pos+risultato).getName(),
+                        JOptionPane.showMessageDialog(null,"il giocatore "+listPl.getCurrentPlayer().getName()+" è finito sulla casella "+deck.get(listPl.getCurrentPlayer().getPosition()).getName(),
                                 "messaggio", 0);
                         
-                        if(deck.get(pos+risultato).getOwner() == listPl.getCurrentPlayer()) {
+                        if(deck.get(listPl.getCurrentPlayer().getPosition()).getOwner() == listPl.getCurrentPlayer()) {
                             buy.setEnabled(false);
                             sell.setEnabled(true);
                             build.setEnabled(true);
-                            if(((Property)deck.get(pos+risultato)).getHotel()) {
+                            if(((Property)deck.get(listPl.getCurrentPlayer().getPosition())).getHotel()) {
                                 build.setEnabled(false);
                             }
                             
-                        } else if(deck.get(pos+risultato).getOwner() == listPl.getPlayerFromIndex(0) && deck.get(pos+risultato).isSalable() ) {
+                        } else if(deck.get(listPl.getCurrentPlayer().getPosition()).getOwner() == listPl.getPlayerFromIndex(0) && deck.get(listPl.getCurrentPlayer().getPosition()).isSalable() ) {
                             buy.setEnabled(true);
                             sell.setEnabled(false);
-                        } else if (deck.get(pos+risultato).getOwner() != listPl.getPlayerFromIndex(0) && deck.get(pos+risultato).isSalable() && deck.get(pos+risultato).getOwner() != listPl.getCurrentPlayer()) {
-                            deck.get(pos+risultato).action(listPl.getCurrentPlayer());
+                        } else if (deck.get(listPl.getCurrentPlayer().getPosition()).getOwner() != listPl.getPlayerFromIndex(0) && deck.get(listPl.getCurrentPlayer().getPosition()).isSalable() && deck.get(listPl.getCurrentPlayer().getPosition()).getOwner() != listPl.getCurrentPlayer()) {
+                            deck.get(listPl.getCurrentPlayer().getPosition()).action(listPl.getCurrentPlayer());
                           //Da togliere
                             JOptionPane.showMessageDialog(null,"il giocatore "+listPl.getCurrentPlayer().getName()+"possiede"+listPl.getCurrentPlayer().getMoney(),
                                     "messaggio", 0);
                             buy.setEnabled(true);
-                        } else if (deck.get(pos+risultato).isSalable() == false) {
-                            deck.get(pos+risultato).action(listPl.getCurrentPlayer());
+                        } else if (deck.get(listPl.getCurrentPlayer().getPosition()).isSalable() == false) {
+                            if(deck.get(listPl.getCurrentPlayer().getPosition()).getName() == "Go To Prison") {
+                                ((Cel)grid.getSouthBox().getComponent(0)).getPositionPawns().resetPawnOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1);    
+                                ((Cel)grid.getNorthBox().getComponent(10)).getPositionPawns().setImageOnIndex(listPl.getIndexFromPlayer(listPl.getCurrentPlayer())-1, listPl.getCurrentPlayer().getPawn());
+                                deck.get(30).action(listPl.getCurrentPlayer());
+                            }
+                            deck.get(listPl.getCurrentPlayer().getPosition()).action(listPl.getCurrentPlayer());
                         }
                         nextPlayer.setEnabled(true);
                         if(listPl.getCurrentPlayer().getMoney() <= 0) {
@@ -150,7 +182,7 @@ public class SouthUtilityButtons extends JPanel {
                                 }
                             }
                           //Da togliere
-                            JOptionPane.showMessageDialog(null,"è il turno di"+ listPl.getCurrentPlayer().getName() + " e si trova sulla casella " + deck.get(listPl.getCurrentPlayer().getPosition()).getName(),
+                            JOptionPane.showMessageDialog(null,"è il turno di "+ listPl.getCurrentPlayer().getName() + " e si trova sulla casella " + deck.get(listPl.getCurrentPlayer().getPosition()).getName(),
                                     "messaggio", 0);
                         }
                     }
