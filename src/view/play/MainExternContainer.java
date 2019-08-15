16d2a07212934fac99279f4a305cd4637dbc0548
Andrea Tassani <andrea.tassani@studio.unibo.it>
2019-08-15 23:52:31 +0200

    javadoc added

--- src/view/play/MainExternContainer.java
+++ src/view/play/MainExternContainer.java
@@ -16,68 +16,77 @@ import javax.swing.JTextArea;
 import model.allTypeOfCard.Entity;
 import model.allTypeOfCard.EntityDeck;
 import model.player.ListOfPlayers;
-
-
-
+/**
+ * 
+ * class that models the frame of play.
+ *
+ */
 public class MainExternContainer extends JFrame {
 	
 	/**
-	 * 
+	 * fields.
 	 */
 	private static Dimension leftSideDimension = new Dimension((int) (Toolkit.getDefaultToolkit().getScreenSize().width / 1.45), Toolkit.getDefaultToolkit().getScreenSize().height);
 	private static Dimension d = new Dimension(Toolkit.getDefaultToolkit().getScreenSize());
+	private static Dimension rightSideDimension = new Dimension(Toolkit.getDefaultToolkit().getScreenSize().width - leftSideDimension.getSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
 	private static Box rightSide = new Box(BoxLayout.Y_AXIS);
 	private static JPanel leftSide = new JPanel(new BorderLayout());
+	private Box extern;
+	private GridCell grid;
 	
 	/**
-	 * @return the rightSide
+	 * constructor.
 	 */
 	public MainExternContainer(ListOfPlayers listPl, ArrayList<Entity> deck) {
 	    
 	        
 
-		Box extern = new Box(BoxLayout.X_AXIS);
+		extern = new Box(BoxLayout.X_AXIS);
 		extern.setSize(Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
 		
 		leftSide.setSize((int) (Toolkit.getDefaultToolkit().getScreenSize().width / 1.45), Toolkit.getDefaultToolkit().getScreenSize().height);
 		leftSide.setPreferredSize(leftSideDimension);
 		leftSide.setBackground(Color.GREEN);
-
-
-		GridCell grid = new GridCell(deck);
+		grid = new GridCell(deck);
 		leftSide.add(new SouthUtilityButtons(listPl, deck, grid), BorderLayout.SOUTH);
 		leftSide.add(grid, BorderLayout.CENTER);
+		
 		for(int i = 0; i<listPl.getNumberPlayer(); i++) {
-		((Cel)grid.getNorthBox().getComponent(0)).getPositionPawns().setImageOnIndex(i, listPl.getPlayerFromIndex(i+1).getPawn());
+		((Cel)grid.getNorthBox().getComponent(0)).getPositionPawns().setImageOnIndex(i, listPl.getPlayerFromIndex(i+1).getPawn());                    //allows to set pawns on start of the game's session
 		}
 		
-		rightSide.setSize(Toolkit.getDefaultToolkit().getScreenSize().width - leftSide.getSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
-		rightSide.setPreferredSize(leftSideDimension);
+		rightSide.setSize(rightSideDimension);
+		rightSide.setPreferredSize(rightSideDimension);
 
 		rightSide.add(new HistoryGUI());
 		rightSide.add(new SituationGUI());
 		extern.add(leftSide);
 		extern.add(rightSide);
-		this.add(extern);
 		
+		this.add(extern);
+		this.setPreferredSize(d);
 		this.setState(JFrame.MAXIMIZED_BOTH);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.setVisible(true);
 		this.pack();
-		this.setState(JFrame.MAXIMIZED_BOTH);
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		
 		//Da togliere
                 JOptionPane.showMessageDialog(null,"è il turno di "+ listPl.getCurrentPlayer().getName() + " e si trova sulla casella " + deck.get(listPl.getCurrentPlayer().getPosition()).getName(),
                         "messaggio", 0);
 	}
-	
+	/**
+	 * 
+	 * @return the dimension of leftside of this frame where it is the playground.
+	 */
 	public static JPanel getLeftSide() {
 		JPanel i = leftSide;
 		return i;
 	}
-	
+	/**
+	 * 
+	 * @return the dimension of rightside of this frame where it is the playground.
+	 */
 	public static Box getRightSide() {
 		Box i = rightSide;
 		return i;
